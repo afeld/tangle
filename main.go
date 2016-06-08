da420f18bffc1166a5cde34d3d62e7f2e2a51eb4
Aidan Feldman <aidan.feldman@gsa.gov>
2016-06-07 21:53:34 -0400

    move scanner to its own package

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/url"
 	"os"
-	"sync"
-	"sync/atomic"
 
-	"github.com/afeld/tangle/models"
+	"github.com/afeld/tangle/scanner"
 )
 
 func startURL() (u *url.URL) {
@@ -27,49 +24,9 @@ func startURL() (u *url.URL) {
 	return
 }
 
-func checkLink(source *url.URL, link models.Link, wg *sync.WaitGroup, numBrokenLinks *uint32) {
-	defer wg.Done()
-
-	if !link.IsValid() {
-		// https://gobyexample.com/atomic-counters
-		atomic.AddUint32(numBrokenLinks, 1)
-
-		dest, _ := link.DestURL()
-		fmt.Printf("%s line %d has broken link to %s.\n", source.String(), link.Node.LineNumber(), dest)
-	}
-}
-
-func scanLinks(source *url.URL, links []models.Link) (numBroken uint32) {
-	var wg sync.WaitGroup
-
-	for _, link := range links {
-		wg.Add(1)
-		go checkLink(source, link, &wg, &numBroken)
-	}
-	wg.Wait()
-
-	return
-}
-
-func scanPage(source *url.URL) (err error) {
-	fmt.Println("Checking for broken links...")
-
-	page := models.Page{AbsURL: source}
-	links, err := page.GetLinks()
-	if err != nil {
-		return
-	}
-
-	fmt.Printf("Number of links found: %d\n", len(links))
-	numBrokenLinks := scanLinks(source, links)
-	fmt.Printf("Number of broken links: %d\n", numBrokenLinks)
-
-	return
-}
-
 func main() {
 	source := startURL()
-	err := scanPage(source)
+	err := scanner.ScanPage(source)
 	if err != nil {
 		log.Fatalln(err)
 	}
